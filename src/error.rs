2c89f2bfbe91f69ba1c769b2df8643850d699ed5
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-11 17:57:53 -0800

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -14,14 +14,26 @@ pub enum MainError {
     Args(#[from] ArgError),
     #[error("{0}")]
     ColorEyre(#[from] color_eyre::Report),
+    #[error("{0}")]
+    JoinError(#[from] tokio::task::JoinError),
 }
 
 #[derive(Error, Debug)]
 pub enum ArgError {
     #[error("{0}")]
     Io(#[from] std::io::Error),
-    #[error("--url argument: `{0}` is not a valid url\nReason: {1}".yellow)]
-    InvalidUrl(String, String),
-    #[error("--url argument: `{0}` is not a supported site\nrun with --help for a list of supported sites.")]
+
+    #[error(
+        "--url argument: {url} is not a valid url\n\
+             Reason: {reason}\n\
+             Example: {example}"
+    )]
+    InvalidUrl {
+        url: String,
+        reason: String,
+        example: String,
+    },
+
+    #[error("--url argument: {0} is not a supported site\nrun with --help for a list of supported sites.")]
     WebsiteNotSupported(String),
 }

4eb51e7a8e9bbc4aa8a7e7bc3b0b7057bb089ac1
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-11 17:57:54 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,125 @@
 mod args;
 mod error;
+mod mangareader;
 mod test;
 
 use std::{
-    fs::{read_dir, File},
+    fs::{self, File},
     io::{self, Write},
+    path::Path,
     process::{Child, Command},
-    thread::sleep,
-    time::{Duration, Instant},
 };
 
+use crate::mangareader::dl_mangareader;
 use args::get_args;
-use color_eyre::eyre::Result;
+use color_eyre::{eyre::Result, owo_colors::OwoColorize};
 use error::MainError;
-use fantoccini::{wd::Capabilities, Client, Locator};
-use serde_json::json;
+use fantoccini::{wd::Capabilities, Client};
+use serde_json::{json, to_string_pretty};
 use spinners::{Spinner, Spinners};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    color_eyre::install().unwrap();
+
+    #[cfg(target_os = "windows")]
+    let gd_data: &[u8] = include_bytes!("../bin/geckodriver-win.exe");
+    #[cfg(target_os = "macos")]
+    let gd_data: &[u8] = include_bytes!("../bin/geckodriver-macos");
+
     let args = get_args()?;
-    let _ = std::fs::create_dir("./download");
 
-    let mut child = start_gd().expect("failed to start gecko driver");
-    let c = start_client().await.expect("failed to start fantoccini");
+    let child = start_gd(gd_data).expect("failed to start gecko driver");
+    let c: Client = start_client().await.expect("failed to start fantoccini");
+    let mut errs = Vec::new();
 
-    let start = Instant::now();
-    c.goto(&args.url).await?;
-    println!("\n\n goto: {}", &args.url);
-
-    // First click
-    let btn = c
-        .wait()
-        .for_element(Locator::Css("a.rtl-row:nth-child(2)"))
-        .await?;
-    btn.click().await?;
-
-    close_open_window(&c).await?;
-
-    // Second click
-    btn.click().await?;
-
-    println!("counting panels...");
-    let ctr = extract_counter(&c).await? - 1;
-
-    println!("beginning download...");
-    let mut sp = Spinner::new(Spinners::Dots, "Downloading...".into());
-    for i in 0..ctr {
-        download_panel_canvas(i, &c).await?;
-        c.execute("hozNextImage()", vec![]).await?;
-        // Update spinner message to show progress
+    for url in args.urls {
+        match dl_mangareader(&c, url, args.dl_path.as_deref()).await {
+            Ok(_) => {}
+            Err(e) => errs.push(e),
+        };
     }
-
-    sp.stop();
-    let elapsed = start.elapsed();
-    println!("\nDownload complete in {:?}", elapsed);
 
     c.close().await?;
-    child.kill().expect("failed to kill gecko driver.");
+    cleanup(child);
+
+    if !errs.is_empty() {
+        let msg = format!("{} {}", errs.len().bold().red(), "errors occured".red());
+        println!("{msg}");
+        for (i, e) in errs.into_iter().enumerate() {
+            write_log(i, MainError::ColorEyre(e))?;
+        }
+    }
+
     Ok(())
 }
 
-async fn download_img(index: u16, c: &Client) -> Result<(), MainError> {
-    if let Ok(e) = c
+pub fn write_log(i: usize, e: MainError) -> Result<(), io::Error> {
+    let mut f = File::create("manga_dl_errors.log")?;
+    let err = json!({
+        "index": i,
+        "error": e.to_string(),
+    });
+    let err = to_string_pretty(&err)?;
+    f.write_all(err.as_bytes())?;
+    f.flush()?;
+
+    Ok(())
+}
+
+pub fn cleanup(mut child: Child) {
+    let message = format!(
+        "{} {}",
+        "performing cleanup,".yellow(),
+        "do not exit the program!".red().bold()
+    );
+    let mut sp = Spinner::new(Spinners::Triangle, message);
+
+    child.kill().expect("Failed to kill geckodriver: {}");
+
+    child
         .wait()
-        .at_most(Duration::from_millis(2000))
-        .for_element(Locator::Css(
-            "div.ds-item.active:nth-child(2) > div:nth-child(1) > canvas:nth-child(2)",
-        ))
-        .await
-    {
-        if let Some(img_url) = e.attr("src").await? {
-            let res = reqwest::get(img_url).await?;
-            let bytes = res.bytes().await?.to_vec();
-            let mut file = File::create(format!("./download/{index}.png"))
-                .expect("Failed to create screenshot file");
-            file.write_all(&bytes)
-                .expect("Failed to write screenshot data");
-        }
-    }
+        .expect("error while waiting for geckodriver: {}");
 
-    Ok(())
+    #[cfg(target_os = "windows")]
+    fs::remove_file("./temp/gd.exe").expect("failed to remove gd.exe");
+    #[cfg(target_os = "macos")]
+    fs::remove_file("./temp/gd.exe").expect("failed to remove gd executable");
+
+    fs::remove_dir("temp").expect("failed to remove temp dir");
+
+    sp.stop_with_newline();
 }
 
-async fn close_open_window(c: &Client) -> Result<(), MainError> {
-    // Handle the ad by closing the newly opened tab
-    let handles = c.windows().await?;
-    if handles.len() > 1 {
-        for handle in handles.iter().skip(1) {
-            c.switch_to_window(handle.clone()).await?;
-            c.close_window().await?;
-        }
-        c.switch_to_window(handles[0].clone()).await?;
+pub fn start_gd(gd_data: &[u8]) -> Result<Child, std::io::Error> {
+    fs::create_dir_all("temp")?;
+    #[cfg(target_os = "windows")]
+    let temp_path = Path::new("temp/gd.exe");
+    #[cfg(target_os = "macos")]
+    let temp_path = Path::new("temp/gd");
+
+    if !temp_path.exists() {
+        let mut temp_file = File::create_new(temp_path)?;
+        temp_file.write_all(gd_data)?;
+        drop(temp_file);
     }
 
-    Ok(())
-}
+    // // Set execute permission (for UNIX systems)
+    // #[cfg(unix)]
+    // {
+    //     let metadata = std::fs::metadata(&temp_path)?;
+    //     let mut permissions = metadata.permissions();
+    //     permissions.set_mode(0o755); // Make the file executable
+    //     std::fs::set_permissions(&temp_path, permissions)?;
+    // }
 
-async fn _handle_popup(c: &Client) -> Result<(), MainError> {
-    if let Ok(e) = c
-        .find(Locator::Css("div[style*='z-index: 2147483647']"))
-        .await
-    {
-        e.click().await?;
+    let child = Command::new(temp_path).spawn()?;
 
-        c.execute(
-            r#"
-            var ad = document.querySelector("div[style*='z-index: 2147483647']");
-            ad.style.display = 'none';  // or 'visibility: hidden'; to just hide without affecting layout
-            "#,
-            vec![],
-        ).await?;
-    }
-
-    Ok(())
+    Ok(child)
 }
 
 async fn start_client() -> Result<fantoccini::Client, fantoccini::error::NewSessionError> {
-    let _caps: Capabilities = json!({
+    let caps: Capabilities = json!({
         "moz:firefoxOptions": {
             "args": ["-headless"]
         }
@@ -127,115 +129,7 @@ async fn start_client() -> Result<fantoccini::Client, fantoccini::error::NewSess
     .clone();
 
     fantoccini::ClientBuilder::native()
-        //.capabilities(caps)
+        .capabilities(caps)
         .connect("http://localhost:4444")
         .await
 }
-
-async fn extract_counter(c: &Client) -> Result<u16, MainError> {
-    let pgs_elm = c
-        .wait()
-        .for_element(Locator::Css("span.hoz-total-image"))
-        .await?;
-
-    let text = pgs_elm.html(true).await?;
-
-    text.parse::<u16>()
-        .map_err(|_| MainError::ParseCounterElement(text))
-}
-
-async fn download_panel_canvas(index: u16, c: &Client) -> Result<(), MainError> {
-    let index = index + 1;
-
-    if let Ok(canvas) = c
-        .wait()
-        .at_most(Duration::from_millis(2500))
-        .for_element(Locator::Css(
-            format!(
-                "div.ds-item.active:nth-child({index}) > div:nth-child(1) > canvas:nth-child(2)"
-            )
-            .as_str(),
-        ))
-        .await
-    {
-        // Wait until canvas content is rendered (check if the canvas is not just black)
-        let mut canvas_ready = false;
-        let timeout = tokio::time::Instant::now() + Duration::from_secs(5);
-        while tokio::time::Instant::now() < timeout {
-            let data_url = c.execute(
-    r#"
-    var canvas = document.querySelector('div.ds-item.active > div:nth-child(1) > canvas:nth-child(2)');
-    return canvas ? canvas.toDataURL() : null;
-    "#,
-    vec![],
-).await?;
-            // Check if the image is not black
-            if let Some(data_url) = data_url.as_str() {
-                if !data_url.contains("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA") {
-                    // base64 for black image
-                    sleep(Duration::from_millis(350));
-                    canvas_ready = true;
-                    break;
-                }
-            }
-
-            // Check again in 200 ms
-            tokio::time::sleep(Duration::from_millis(200)).await;
-        }
-
-        if canvas_ready {
-            let ss = canvas.screenshot().await?;
-            let mut file = File::create(format!("./download/{index}.png"))
-                .expect("Failed to create screenshot file");
-            file.write_all(&ss)
-                .expect("Failed to write screenshot data");
-        } else {
-            println!("Canvas did not load in time.");
-        }
-    } else {
-        download_img(index, c).await?;
-    }
-
-    Ok(())
-}
-
-pub fn crop_imgs() -> Result<(), Box<dyn std::error::Error>> {
-    for (i, entry) in read_dir("./download")?.enumerate() {
-        let entry = entry?;
-        let path = entry.path();
-
-        let path = path.to_string_lossy();
-
-        Command::new("magick")
-            .arg(path.as_ref()) // Path to the image
-            .arg("-fuzz")
-            .arg("10%")
-            .arg("-fill")
-            .arg("transparent")
-            .arg("-opaque")
-            .arg("rgb(17,17,17)")
-            .arg("-trim")
-            .arg("+repage")
-            .arg(path.as_ref()) // Output file name
-            .spawn()
-            .expect("error launching magick");
-
-        print!("\r{i}");
-    }
-
-    Ok(())
-}
-
-fn start_gd() -> Result<Child, io::Error> {
-    let child = Command::new("./geckodriver").spawn()?;
-    Ok(child)
-}
-
-// fn get_manga_input() -> Result<String, io::Error> {
-//     let mut input = String::new();
-//     println!("\nEnter mangareader volume url.");
-//     println!("Example: https://mangareader.to/read/tokyo-ghoul-108/en/volume-1");
-//     io::stdin().read_line(&mut input)?;
-//
-//     Ok(input)
-// }

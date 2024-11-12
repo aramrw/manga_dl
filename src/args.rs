04cd232819064a178c6fa2f195e043da9dae2e88
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-11 17:57:52 -0800

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -1,41 +1,110 @@
+use std::str::FromStr;
+
 use clap::Parser;
+use color_eyre::owo_colors::OwoColorize;
 
 use crate::error::ArgError;
 
+/// manga_dl Url argument
+#[derive(Debug, Clone)]
+pub struct Url(pub String);
+
+/// Arguments passed to the manga_dl cli
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    #[arg(short, long, num_args = 1..)]
+    pub urls: Vec<Url>,
+    /// Use an absolute path instead of manga_dl exe dir
     #[arg(short, long)]
-    pub url: String,
+    pub dl_path: Option<String>,
+}
+
+impl Args {
+    pub fn check_urls(&mut self) -> Result<(), ArgError> {
+        for url in self.urls.iter_mut() {
+            url.check_url()?;
+        }
+        Ok(())
+    }
 }
 
 pub fn get_args() -> Result<Args, ArgError> {
-    let args = Args::parse();
-    let url = &args.url;
-    check_url(url)?;
+    let mut args = Args::parse();
+    args.check_urls()?;
 
     Ok(args)
 }
 
-pub fn check_url(url: &str) -> Result<(), ArgError> {
-    let url = url.to_lowercase();
+// Implement FromStr for Url so that clap can parse it as an argument
+impl FromStr for Url {
+    type Err = clap::Error;
 
-    if !url.is_ascii() {
-        return Err(ArgError::InvalidUrl(url, "url is not valid ascii".into()));
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut url = Url(s.to_string());
+        url.check_url()
+            .map_err(|e| clap::Error::raw(clap::error::ErrorKind::InvalidValue, e))?;
+        Ok(url)
     }
-    if url.is_empty() {
-        return Err(ArgError::InvalidUrl(url, "empty string".into()));
-    }
-    if !url.contains(".to") || !url.contains(".com") {
-        return Err(ArgError::InvalidUrl(
-            url,
-            "url is missing a valid top level domain\nExample: *.com | *.to".into(),
-        ));
+}
+
+impl Url {
+    pub fn get_title_from_url(&self) -> Option<String> {
+        if self.0.contains("mangareader") {
+            if let Some(start) = self.0.split_once("/read/") {
+                // Extract the part after "/read/" until the next "/"
+                return Some(start.1.replace("/", "-").to_string());
+            }
+        }
+        None
     }
 
-    if !url.contains("mangareader.to") {
-        return Err(ArgError::WebsiteNotSupported(url));
-    }
+    pub fn check_url(&mut self) -> Result<(), ArgError> {
+        if !self.0.contains("https://") {
+            let new_url = format!("https://{}", self.0);
+            self.0 = new_url;
+        }
 
-    Ok(())
+        let url = self.0.to_lowercase();
+
+        if !url.is_ascii() {
+            return Err(ArgError::InvalidUrl {
+                url,
+                reason: "URL is not valid ASCII".bold().to_string(),
+                example: "*.com | *.to".yellow().to_string(),
+            });
+        }
+
+        if url.is_empty() {
+            return Err(ArgError::InvalidUrl {
+                url,
+                reason: "Empty string".bold().to_string(),
+                example: "*.com | *.to".yellow().to_string(),
+            });
+        }
+
+        if !(url.contains(".to") || url.contains(".com")) {
+            return Err(ArgError::InvalidUrl {
+                url,
+                reason: "URL is missing a valid top-level domain".bold().to_string(),
+                example: "*.com | *.to".yellow().to_string(),
+            });
+        }
+
+        if !url.contains("mangareader.to") {
+            return Err(ArgError::WebsiteNotSupported(url));
+        }
+
+        if url.contains("mangareader.to") && !url.contains("/read") {
+            return Err(ArgError::InvalidUrl {
+                url,
+                reason: "mangareader URL is missing /read".bold().to_string(),
+                example: "mangareader.to/read/vagabond-4/ja/chapter-6"
+                    .yellow()
+                    .to_string(),
+            });
+        }
+
+        Ok(())
+    }
 }

00e3699c4f36e62e1d7f422389ab31535332867e
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-01 21:15:59 -0800

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use base64::DecodeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -6,8 +7,6 @@ pub enum MainError {
     IoErr(#[from] std::io::Error),
     #[error("{0}")]
     Fantoccini(#[from] fantoccini::error::CmdError),
-    #[error("{0}")]
-    Reqwest(#[from] reqwest::Error),
     #[error("{0} could not be parsed")]
     ParseCounterElement(String),
     #[error("{0}")]
@@ -16,6 +15,33 @@ pub enum MainError {
     ColorEyre(#[from] color_eyre::Report),
     #[error("{0}")]
     JoinError(#[from] tokio::task::JoinError),
+    #[error("{0}")]
+    DownloadImage(#[from] DownloadImageError),
+}
+
+#[warn(dead_code)]
+#[derive(Error, Debug)]
+pub enum DownloadImageError {
+    #[error("{0}")]
+    ColorEyre(#[from] color_eyre::Report),
+    #[error("{0}")]
+    Base64(#[from] DecodeError),
+    #[error("toDataUrl() executed on the canvas returned an invalid url: {0}")]
+    InvalidDataUrl(String),
+    #[error("{0}")]
+    CanvasScript(String),
+    #[error("{0}")]
+    Fantoccini(#[from] fantoccini::error::CmdError),
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("get request failed for src url: {0}; reason: {1}")]
+    GetReqwest(String, String),
+    #[error("could not get canvas element from selector: {0}")]
+    MissingCanvasElement(String),
+    #[error("failed to find an image for the current index of: {0}")]
+    MissingImgElement(String),
 }
 
 #[derive(Error, Debug)]

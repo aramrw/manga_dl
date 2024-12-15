12291a4730a3deda83b4c8a0074aa585252af945
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-15 14:40:41 -0800

    Update mangagun.rs

--- src/mangagun.rs
+++ src/mangagun.rs
@@ -1,80 +1,42 @@
-use std::{collections::HashSet, time::Instant};
+use std::{collections::HashSet, io::Write, time::Instant};
 
-use color_eyre::{owo_colors::OwoColorize, Result};
+use color_eyre::{eyre::Context, Result};
 use fantoccini::{Client, Locator};
 use spinners::{Spinner, Spinners};
 use tokio::time::sleep;
 
 use crate::{
-    args::{Args, Url},
-    loading::{downloading_panel_data_msg, print_download_complete_msg, print_indexes_arg},
-    mangareader::{gen_rand, write_img, ImageData, _handle_popup},
+    cli::{Cli, LogLevel, Url},
+    g_handle_popup,
+    loading::{downloading_panel_data_msg, print_download_complete_msg},
+    mangareader::{write_img, ImageData},
+    setup_nav,
 };
 
-pub async fn dl_mangagun(client: &Client, url: &Url, args: &Args) -> Result<()> {
-    let title = url.title.clone().unwrap_or_else(|| gen_rand().to_string());
+pub type NavigateGroup = (String, String, Spinner);
 
-    let dl_path = match &args.dl_path {
-        Some(p) => format!("{p}/{}", title),
-        None => {
-            format!("./download/{title}")
-        }
-    };
-
-    std::fs::create_dir_all(&dl_path)
-        .unwrap_or_else(|_| panic!("could not create dir_all @: {dl_path}"));
-
+pub async fn dl_mangagun(client: &Client, url: &Url, args: &Cli) -> Result<()> {
+    let (_, dl_path, mut sp) = setup_nav(client, url, args).await?;
     let start = Instant::now();
-    println!();
-    let message = format!("{} -> {}", "navigating to", title.bold().yellow());
 
-    let mut sp = Spinner::new(Spinners::Arc, message);
-    client.goto(&url.url).await?;
-    sp.stop_with_newline();
-
-    // hide the navbar so it doesnt interferere with the ss
-    client
-        .execute(r#"
-        const nav = document.querySelector(".navbar");
-        if (nav) {
-            nav.style.display = 'none';
-        }"#,
-            vec![],
-        )
-        .await?;
-
-    // hide the bottom navbar so it does't interfere with the ss
-    client
-        .execute(r#"
-        const bottom_nav = document.getElementById("rd-side_icon");
-        if (bottom_nav) {
-            bottom_nav.style.display = 'none';
-        }"#,
-            vec![],
-        )
-        .await?;
-
-
-    if let Some(indexes) = &args.indexes {
-        print_indexes_arg(indexes);
-    }
+    // hide the top-navbar
+    execute_set_element_hidden_inline(client, ".navbar").await?;
+    // hide the bottom-navbar
+    execute_set_element_hidden_inline(client, "#rd-side_icon").await?;
 
     let index_map: Option<HashSet<&usize>> =
         args.indexes.as_ref().map(|slice| slice.iter().collect());
-    let img_data = get_all_images(&dl_path, client, &mut sp, index_map, args.debug).await?;
+    let img_data = get_all_images(&dl_path, client, &mut sp, index_map, &args.log).await?;
 
     let elapsed = start.elapsed();
     print_download_complete_msg(elapsed);
 
-    sp = Spinner::new(
-        Spinners::Triangle,
-        "writing data to file..".yellow().to_string(),
-    );
+    //sp = Spinner::new(Spinners::Triangle, "".into());
 
     img_data
         .into_iter()
-        .for_each(|data| write_img(data).unwrap());
-    sp.stop_with_newline();
+        .for_each(|data| write_img(&data).unwrap());
+    //sp.stop_with_newline();
 
     Ok(())
 }
@@ -84,31 +46,40 @@ async fn get_all_images(
     c: &Client,
     sp: &mut Spinner,
     index_map: Option<HashSet<&usize>>,
-    is_debug: bool,
+    log: &LogLevel,
 ) -> Result<HashSet<ImageData>> {
-    _handle_popup(c).await?;
+    g_handle_popup(c).await.wrap_err(line!())?;
     let imgs = c.find_all(Locator::Css("img.chapter-img")).await?;
     let mut new_imgs: HashSet<ImageData> = HashSet::new();
 
     let max = imgs.len();
+    // if the index map is Some, skip indexes that aren't specified
     for (i, img) in imgs.into_iter().enumerate() {
-        // if the index map is some skip indexes that aren't specified
         if let Some(i_map) = &index_map {
             if !i_map.contains(&i) {
                 continue;
             }
         }
-        hide_ad_modal(c).await?;
+        execute_set_element_hidden_inline(c, "#adModal").await?;
+        execute_set_element_hidden_computed(c).await?;
 
         // wait until the image src is not the loading GIF
         while let Some(src) = img.attr("src").await? {
             if !src.contains("gif") {
                 let rect = img.rectangle().await?;
-                // the spinner gif is 100 x 99.999 repeating.
-                if rect.2 > 150.0 {
-                    is_debug.then(|| println!("\ndl img_src: {src}"));
-                    break;
+                // the spinner gif is 100 x 99.9...
+                if rect.2 > 300.0 {
+                    match log {
+                        LogLevel::Full => {
+                            print!(":\n  {src}");
+                            std::io::stdout().flush().expect("failed to flush output");
+                        }
+                        _ => { /* skip */ }
+                    }
+                    let msg = downloading_panel_data_msg(i as u16, max as u16);
+                    *sp = Spinner::new(Spinners::Arc, msg);
                 }
+                break;
             }
             sleep(std::time::Duration::from_millis(300)).await;
         }
@@ -118,27 +89,37 @@ async fn get_all_images(
         let img = ImageData { bytes, path };
 
         new_imgs.insert(img);
-
-        let msg = downloading_panel_data_msg(i as u16, max as u16);
-        if !is_debug {
-            *sp = Spinner::new(Spinners::Arc, msg);
-        }
     }
 
-    sp.stop_with_newline();
     Ok(new_imgs)
 }
 
-pub async fn hide_ad_modal(c: &Client) -> Result<()> {
-    c.execute(
-        r#"
-        const adModal = document.getElementById('adModal');
-        if (adModal) {
-            adModal.style.display = 'none';  
-        }
-        "#,
-        vec![],
-    ).await?;
+pub async fn execute_set_element_hidden_computed(c: &Client) -> Result<()> {
+    let script = r#"
+    var elements = document.querySelectorAll('*');
+    elements.forEach(function(element) {
+    var style = window.getComputedStyle(element);
+    if (style.zIndex === '2147483647') {
+        element.style.display = 'none';
+    }});
+    "#;
+    c.execute(script, vec![]).await?;
+    Ok(())
+}
 
+/// * selector
+///
+/// ".navbar" || "#navbar";
+pub async fn execute_set_element_hidden_inline(c: &Client, selector: &str) -> Result<()> {
+    let script = format!(
+        r#"
+            var element = document.querySelector('{}');
+            if (element) {{
+                element.style.display = 'none';
+            }} 
+            "#,
+        selector
+    );
+    c.execute(&script, vec![]).await?;
     Ok(())
 }

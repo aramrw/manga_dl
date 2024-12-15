c164ea590624fe422a0470f54573c0cb2dafaedf
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-15 14:40:42 -0800

    Update mangareader.rs

--- src/mangareader.rs
+++ src/mangareader.rs
@@ -1,35 +1,31 @@
 use base64::prelude::*;
-use serde_json::Value;
-use std::{
-    collections::HashSet,
-    fs::{read_dir, File},
-    io::Write,
-    time::{Duration, Instant},
-};
+use std::{collections::HashSet, fs::read_dir, time::Duration};
 
 use crate::{
-    args::Url,
-    error::DownloadImageError,
-    loading::{indexes_failed_msg, print_download_complete_msg, print_reqerr_count},
-    write_log, LogError,
+    cli::{Cli, Url},
+    error::{DownloadImageError, MangaReaderError},
+    g_close_open_window,
+    loading::print_reqerr_count,
+    setup_nav,
 };
 use crate::{error::MainError, loading::downloading_panel_data_msg};
 use color_eyre::{
-    eyre::{Context, Result},
+    eyre::{eyre, Context, Result},
     owo_colors::OwoColorize,
+    Section,
 };
 use fantoccini::{Client, Locator};
 use reqwest::{Client as ReqClient, ClientBuilder as ReqClientBuilder};
 use spinners::{Spinner, Spinners};
 use std::process::Command;
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq, Debug)]
 pub struct ImageData {
     pub bytes: Vec<u8>,
     pub path: String,
 }
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq, Debug)]
 pub struct ReqImageData {
     pub url: String,
     pub path: String,
@@ -37,45 +33,15 @@ pub struct ReqImageData {
 
 /// Arguments
 ///
-/// * `index` - mangareader has a popup if youve have no cookies,
-///
-/// however on consequent loads its
-/// not necessary to deal with.
-pub async fn dl_mangareader(
-    client: &Client,
-    url: &Url,
-    dl_path: Option<&str>,
-    index: usize,
-) -> Result<()> {
-    let title = url.title.clone()
-        .unwrap_or_else(|| gen_rand().to_string());
-
-    let dl_path = match dl_path {
-        Some(p) => format!("{p}/{}", title),
-        None => {
-            format!("./download/{title}")
-        }
-    };
-
-    std::fs::create_dir_all(&dl_path)
-        .unwrap_or_else(|_| panic!("could not create dir_all @: {dl_path}"));
-
-    let start = Instant::now();
-    println!();
-    let message = format!("{} -> {}", "navigating to", title.bold().yellow());
-
-    let mut sp = Spinner::new(Spinners::Arc, message);
-    client.goto(&url.url).await?;
+/// * `index` - needed because mangareader has a popup on render,
+pub async fn dl_mangareader(client: &Client, url: &Url, args: &Cli, index: usize) -> Result<()> {
+    let (title, dl_path, mut _sp) = setup_nav(client, url, args).await?;
+    //let start = Instant::now();
 
     if index == 0 {
         select_reading_mode(client).await?;
     }
-
-    sp.stop_with_newline();
-
-    sp = Spinner::new(Spinners::Arc, "counting panels".yellow().to_string());
-    let max = extract_counter(client).await? - 1;
-    sp.stop_with_newline();
+    let max = count_pages(client).await? - 1;
 
     // hold all the bytes and formatted paths of the imgs
     // to write all at once at the very end
@@ -89,102 +55,84 @@ pub async fn dl_mangareader(
     // (url, dl_path)
     let mut src_urls: HashSet<ReqImageData> = HashSet::new();
     let is_imgs = _find_images(client).await;
-    let mut indexes_failed = HashSet::new();
+    let mut errors = Vec::with_capacity(max.into());
 
-    println!();
     for i in 0..max {
+        if errors.len() > 3 {
+            break;
+        }
         if !is_imgs {
-            match download_panel_canvas(i, client, &dl_path, &mut img_data_vec, Duration::from_secs(5)).await {
+            match download_panel_canvas(
+                i,
+                client,
+                &dl_path,
+                &mut img_data_vec,
+                Duration::from_secs(5),
+            )
+            .await
+            {
                 Ok(_) => (),
                 Err(DownloadImageError::MissingCanvasElement(_)) => {
-                    if download_panel_img(i, client, &dl_path, &mut src_urls)
-                        .await
-                        .is_err()
-                    {
-                        indexes_failed.insert(i);
+                    if let Err(e) = download_panel_img(i, client, &dl_path, &mut src_urls).await {
+                        errors.push(e);
                     }
                 }
                 Err(e) => return Err(e.into()),
             };
         } else if let Err(img_err) = download_panel_img(i, client, &dl_path, &mut src_urls).await {
-            if let Err(canvas_err) =
-                download_panel_canvas(i, client, &dl_path, &mut img_data_vec, Duration::from_secs(5)).await
+            if let Err(canvas_err) = download_panel_canvas(
+                i,
+                client,
+                &dl_path,
+                &mut img_data_vec,
+                Duration::from_secs(5),
+            )
+            .await
             {
-                let err = LogError {
-                    url: dl_path.to_string(),
-                    index: i as usize,
-                    error: format!("{} + {}", img_err, canvas_err),
-                };
-                write_log(err)?;
+                errors.push(canvas_err);
+                errors.push(img_err);
             };
         }
-        sp = Spinner::new(Spinners::Dots3, downloading_panel_data_msg(i, max));
+        _sp = Spinner::new(Spinners::Dots3, downloading_panel_data_msg(i, max));
         client.execute("hozNextImage()", vec![]).await?;
     }
-    sp.stop_with_newline();
-    let mut reqerr_count = 0;
-
-    if !indexes_failed.is_empty() {
-        sp = Spinner::new(Spinners::Dots3, indexes_failed_msg(indexes_failed.len()));
-        client.goto(&url.url).await?;
-        for index in 0..max {
-            if indexes_failed.contains(&index) {
-                match download_panel_canvas(index, client, &dl_path, &mut img_data_vec, Duration::from_secs(10)).await {
-                    Ok(_) => (),
-                    Err(DownloadImageError::MissingCanvasElement(e)) => {
-                        let err = LogError {
-                            url: dl_path.to_string(),
-                            index: index as usize,
-                            error: format!("failed again on the second attempt to download: {}", e),
-                        };
-                        write_log(err)?;
-                        reqerr_count += 1;
-                    }
-                    Err(e) => return Err(e.into()),
-                };
-            }
-            client.execute("hozNextImage()", vec![]).await?;
-        }
-
-        sp.stop_with_newline();
-    }
+    _sp.stop_with_newline();
+    let mut error_reports: Vec<color_eyre::Report> = Vec::with_capacity(src_urls.len());
 
     if !src_urls.is_empty() {
         for (i, img) in src_urls.into_iter().enumerate() {
-            let res = download_img_src(&img.url, img.path, req_client.clone()).await;
+            let res = download_img_src(&img.url, img.path, &req_client).await;
 
             match res {
                 Ok(r) => img_data_vec.push(r),
                 Err(e) => {
-                    reqerr_count += 1;
-                    let log_err = LogError {
-                        url: img.url,
-                        index: i,
-                        error: e.to_string(),
-                    };
-                    let _ = write_log(log_err);
+                    let report = eyre!("failed on `{}. {}`: \n{}", i, img.url, e.to_string());
+                    error_reports.push(report);
                 }
             }
         }
     }
-    //sp.stop_with_newline();
 
-    if reqerr_count > 0 {
-        print_reqerr_count(reqerr_count, &title);
+    //_sp.stop_with_newline();
+
+    if !error_reports.is_empty() {
+        print_reqerr_count(error_reports.len(), &title);
+    } else if !errors.is_empty() {
+        print_reqerr_count(errors.len(), &title);
+        println!("{}", "STDERROR:\n".bright_red());
+        for e in errors.into_iter() {
+            eprintln!("{}", e.red());
+        }
     }
 
-    let elapsed = start.elapsed();
-    print_download_complete_msg(elapsed);
+    //let elapsed = start.elapsed();
+    //print_download_complete_msg(elapsed);
 
-    sp = Spinner::new(
-        Spinners::Triangle,
-        "writing data to file..".yellow().to_string(),
-    );
+    //_sp = Spinner::new(Spinners::Triangle, style_text!("writing data to file.."));
 
     img_data_vec
         .into_iter()
-        .for_each(|data| write_img(data).unwrap());
-    sp.stop_with_newline();
+        .for_each(|data| write_img(&data).unwrap());
 
     Ok(())
 }
@@ -291,7 +239,8 @@ async fn download_panel_img(
             }
         }
     } else {
-        return Err(DownloadImageError::MissingImgElement(dl_path.to_string()));
+        let path = format!("{index}.jpg");
+        return Err(DownloadImageError::MissingImgElement(path));
     }
 
     Ok(())
@@ -300,7 +249,7 @@ async fn download_panel_img(
 pub async fn download_img_src(
     url: &str,
     path: String,
-    c: ReqClient,
+    c: &ReqClient,
 ) -> Result<ImageData, DownloadImageError> {
     let res = c
         .get(url)
@@ -317,12 +266,17 @@ pub async fn download_img_src(
     Ok(img)
 }
 
-async fn extract_counter(c: &Client) -> Result<u16, MainError> {
+async fn count_pages(c: &Client) -> Result<u16, MainError> {
+    let selector = Locator::Css("span.hoz-total-image");
     let pgs_elm = c
         .wait()
-        .at_most(Duration::from_millis(5000))
-        .for_element(Locator::Css("span.hoz-total-image"))
-        .await?;
+        .at_most(Duration::from_millis(1000))
+        .for_element(selector)
+        .await
+        .map_err(|e| {
+            eyre!("total pages element not found:\n  {}{:?}", e, selector)
+                .with_note(|| "try running with -- --full")
+        })?;
 
     let text = pgs_elm.html(true).await?;
 
@@ -330,12 +284,12 @@ async fn extract_counter(c: &Client) -> Result<u16, MainError> {
         .map_err(|_| MainError::ParseCounterElement(text))
 }
 
-pub fn write_img(data: ImageData) -> Result<()> {
-    let mut file = File::create(&data.path)?;
-    file.write_all(&data.bytes)?;
+pub fn write_img(data: &ImageData) -> Result<()> {
+    std::fs::write(&data.path, &data.bytes)?;
     Ok(())
 }
 
+#[allow(dead_code)]
 pub fn crop_imgs(dl_path: &str) -> Result<(), MainError> {
     for (i, entry) in read_dir(dl_path)?.enumerate() {
         let entry = entry?;
@@ -348,7 +302,7 @@ pub fn crop_imgs(dl_path: &str) -> Result<(), MainError> {
             .arg("-fuzz")
             .arg("10%")
             .arg("-fill")
-            .arg("transparent")
+            .arg("tran_sparent")
             .arg("-opaque")
             .arg("rgb(17,17,17)")
             .arg("-trim")
@@ -364,58 +318,22 @@ pub fn crop_imgs(dl_path: &str) -> Result<(), MainError> {
     Ok(())
 }
 
-pub fn gen_rand() -> i32 {
-    let num = vec![2, 3, 50, 80, 23124];
-    let add = &num as *const Vec<i32>;
-    add as i32
-}
-
-async fn select_reading_mode(c: &Client) -> Result<()> {
+/// exists because mangareader asks new profiles to select the orientation of the reader
+async fn select_reading_mode(c: &Client) -> Result<(), MangaReaderError> {
     if let Ok(btn) = c
         .wait()
         .at_most(Duration::from_millis(2000))
         .for_element(Locator::Css("a.rtl-row:nth-child(2)"))
         .await
     {
+        btn.click()
+            .await
+            .with_error(|| MangaReaderError::SelectReadingMode {
+                info: "".to_string(),
+            })?;
+        g_close_open_window(c).await?;
+
         btn.click().await?;
-
-        close_open_window(c).await?;
-
-        // Second click
-        btn.click().await?;
-    }
-
-    Ok(())
-}
-
-async fn close_open_window(c: &Client) -> Result<(), MainError> {
-    // Handle the ad by closing the newly opened tab
-    let handles = c.windows().await?;
-    if handles.len() > 1 {
-        for handle in handles.iter().skip(1) {
-            c.switch_to_window(handle.clone()).await?;
-            c.close_window().await?;
-        }
-        c.switch_to_window(handles[0].clone()).await?;
-    }
-
-    Ok(())
-}
-
-pub async fn _handle_popup(c: &Client) -> Result<(), MainError> {
-    if let Ok(e) = c
-        .find(Locator::Css("div[style*='z-index: 2147483647']"))
-        .await
-    {
-        e.click().await?;
-
-        c.execute(
-            r#"
-            var ad = document.querySelector("div[style*='z-index: 2147483647']");
-            ad.style.display = 'none';  // or 'visibility: hidden'; to just hide without affecting layout
-            "#,
-            vec![],
-        ).await?;
     }
 
     Ok(())
